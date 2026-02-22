438ec5a939d41559666a96da76a91fc7dfd323b2
jdk829355 <jdk829355@gmail.com>
2026-02-22 19:17:15 +0900

    fix: main_server에서 변경한 db 테이블 이름 반영

--- models/db_models.py
+++ models/db_models.py
@@ -1,17 +1,17 @@
-from sqlalchemy import Column, String, Text, Integer, DateTime, Boolean, ForeignKey, Float, Index
+from sqlalchemy import Column, String, Text, Integer, DateTime, Boolean, ForeignKey
 from services.service_db import Base
 from sqlalchemy.sql import func
 
 class Keyword(Base):
-    __tablename__ = 'keywords'
+    __tablename__ = 'KEYWORDS'
     id = Column(Integer, primary_key=True)
     keyword = Column(String(100), unique=True, nullable=False) # 중복 방지
     
     # 유사어 통합용 (예: k8s의 master_id를 Kubernetes의 id로 설정)
-    master_id = Column(Integer, ForeignKey('keywords.id'), nullable=True)
+    master_id = Column(Integer, ForeignKey('KEYWORDS.id'), nullable=True)
 
 class ExternalPost(Base):
-    __tablename__ = 'external_posts'
+    __tablename__ = 'EXTERNAL_POSTS'
     id = Column(String(64), primary_key=True) # URL 해시값
     source = Column(String(50))               # 기업명
     title = Column(String(255))               # 글 제목
@@ -24,13 +24,13 @@ class ExternalPost(Base):
     # 분석 상태 및 결과
     is_analyzed = Column(Boolean, default=False, index=True)
     summary = Column(Text, nullable=True)
-    field_id = Column(Integer, ForeignKey('fields.id',ondelete='CASCADE', onupdate='CASCADE'), nullable=True, index=True)
+    field_id = Column(Integer, ForeignKey('FIELDS.id',ondelete='CASCADE', onupdate='CASCADE'), nullable=True, index=True)
 class PostKeywordMapping(Base):
-    __tablename__ = 'external_posts_keywords'
-    keyword_id = Column(Integer, ForeignKey('keywords.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
-    post_id = Column(String(64), ForeignKey('external_posts.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
+    __tablename__ = 'EXTERNAL_POSTS_KEYWORDS'
+    keyword_id = Column(Integer, ForeignKey('KEYWORDS.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
+    post_id = Column(String(64), ForeignKey('EXTERNAL_POSTS.id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
 
 class Fields(Base):
-    __tablename__ = 'fields'
+    __tablename__ = 'FIELDS'
     id = Column(Integer, primary_key=True, autoincrement=True, default=1)
     name = Column(String(50), unique=True, nullable=False)
